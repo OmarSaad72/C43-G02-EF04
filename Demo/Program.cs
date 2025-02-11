2ac49757f6087b31aac0b2700f98427336a0f34c
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-02-11 20:08:07 +0200

    LINQ Inner Join

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -70,15 +70,67 @@ namespace Demo
             //{
             //    Console.WriteLine(employee?.EmpName ?? "Not Found");
             //}
-            var department = (from d in dbContext.Departments
-                              where d.DeptId == 1
-                              select d).FirstOrDefault();
-            if (department != null)
+            //var department = (from d in dbContext.Departments
+            //                  where d.DeptId == 1
+            //                  select d).FirstOrDefault();
+            //if (department != null)
+            //{
+            //    Console.WriteLine($"{department?.DeptId} & {department?.Name ?? "Not Found"}");
+            //    foreach (var item in department.Employees)
+            //    {
+            //        //Console.WriteLine(item.EmpName);   
+            //    }
+            //}
+            #endregion
+            #endregion
+            #region LINQ Operators
+            #region LINQ Inner Join
+            // Inner Join: 
+            //var result = from E in dbContext.Employees
+            //             join D in dbContext.Departments
+            //             on E.DeptId equals D.DeptId
+            //             select new
+            //             {
+            //                 D.DeptId,
+            //                 D.Name,
+            //                 E.EmpId,
+            //                 E.EmpName
+            //             };
+            // Employees ==> Outer & Department ==> Inner
+            //result = dbContext.Employees.Join(dbContext.Departments, e => e.DeptId, d => d.DeptId, (e, d) => new
+            //{
+            //    d.DeptId,
+            //    d.Name,
+            //    e.EmpId,
+            //    e.EmpName
+            //});
+            //foreach (var item in result)
             {
-                Console.WriteLine($"{department?.DeptId} & {department?.Name ?? "Not Found"}");
-                foreach (var item in department.Employees)
+                //Console.WriteLine(item);
+            }
+
+            // Group Join: 
+            var result = dbContext.Departments.GroupJoin(dbContext.Employees, d => d.DeptId, e => e.DeptId,
+                (dept, emp) => new
                 {
-                    //Console.WriteLine(item.EmpName);   
+                    Department = dept,
+                    Employees = emp
+                }).Where(z => z.Employees.Count() > 0);
+            result = (from d in dbContext.Departments
+                     join e in dbContext.Employees
+                     on d.DeptId equals e.DeptId
+                     into z
+                     select new
+                     {  
+                         Department = d,
+                         Employees = z,
+                     }).Where(z => z.Employees.Count() > 0);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Department.Name);
+                foreach (var item1 in item.Employees)
+                {
+                    Console.WriteLine(item1.EmpName);
                 }
             }
             #endregion
