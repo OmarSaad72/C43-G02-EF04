e0e8e104219627b98dca87c48dba77625b2804a6
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-02-11 17:31:50 +0200

    Explicit Loading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -13,14 +13,33 @@ namespace Demo
             #endregion
             #region Loading
             #region NavigationalProperty Loading
+            //var employee = (from e in dbContext.Employees
+            //               where e.DeptId == 1
+            //               select e).FirstOrDefault();
+            ////Console.WriteLine(employee?.EmpName??"Not Found");
+            //var department = (from d in dbContext.Departments
+            //                  where d.DeptId == 1
+            //                  select d).FirstOrDefault();
+            ////Console.WriteLine(department?.Name ?? "Not Found");
+            #endregion
+            #region Explicit Loading
             var employee = (from e in dbContext.Employees
-                           where e.DeptId == 1
-                           select e).FirstOrDefault();
-            //Console.WriteLine(employee?.EmpName??"Not Found");
+                            where e.DeptId == 1
+                            select e).FirstOrDefault();
+            if (employee != null)
+            {
+                dbContext.Entry(employee).Reference(d => d.Department).Load();
+                //Console.WriteLine(employee?.EmpName??"Not Found");
+            }
             var department = (from d in dbContext.Departments
                               where d.DeptId == 1
                               select d).FirstOrDefault();
-            //Console.WriteLine(department?.Name ?? "Not Found");
+            //dbContext.Entry(employee).Reference(d => d.Department).Load();
+            if (department != null)
+            {
+                dbContext.Entry(department).Collection(d => d.Employees).Load();
+                //Console.WriteLine(employee?.EmpId + department?.Name ?? "Not Found");
+            }
             #endregion
             #endregion
         }
