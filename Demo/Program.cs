7a792be24e3f28bf25e8fdd64110ebf861c891e4
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-02-11 17:06:56 +0200

    NavigationalProperty Loading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,8 +8,20 @@ namespace Demo
         static void Main()
         {
             #region Data Seeding
-            //using CompanyDbContext dbContext = new CompanyDbContext();
+            using CompanyDbContext dbContext = new CompanyDbContext();
             //CompanyDbContextSeed.Seed(dbContext);
+            #endregion
+            #region Loading
+            #region NavigationalProperty Loading
+            var employee = (from e in dbContext.Employees
+                           where e.DeptId == 1
+                           select e).FirstOrDefault();
+            //Console.WriteLine(employee?.EmpName??"Not Found");
+            var department = (from d in dbContext.Departments
+                              where d.DeptId == 1
+                              select d).FirstOrDefault();
+            //Console.WriteLine(department?.Name ?? "Not Found");
+            #endregion
             #endregion
         }
     }
